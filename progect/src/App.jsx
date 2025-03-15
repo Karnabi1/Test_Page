18c571859321ceeaa9525c200a6faae8264401cc
Sofiia <nazarenkosofia664@gmail.com>
2025-03-15 19:09:30 +0200

    Написала код для Footer.jsx

--- progect/src/App.jsx
+++ progect/src/App.jsx
@@ -2,52 +2,18 @@ import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import Footer from './components/Footer';
 
-function ContactForm() {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [message, setMessage] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    // Логіка для надсилання повідомлення
-  };
+function App() {
+  const [count, setCount] = useState(0);
 
   return (
-    <form onSubmit={handleSubmit} className="contact-form">
-      <div>
-        <label htmlFor="name">Ім'я:</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="email">Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          required
-        />
-      </div>
-      <div>
-        <label htmlFor="message">Повідомлення:</label>
-        <textarea
-          id="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-          required
-        ></textarea>
-      </div>
-      <button type="submit">Надіслати</button>
-    </form>
+    <> 
+      <div className="App">
+      <Footer />
+    </div>
+    </>
   );
 }
 
-export default ContactForm;
-
+export default App; 
---
+++ progect/src/components/Footer.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2025 Всі права захищені</p>
+      <div className="icons">
+        <div className="icon-container">
+          <img className="background-icon" src="\images\Polygon-1-copy-2 2.png" alt="background" />
+          <img className="overlay-icon" src="\images\fe_instagram.png" alt="Instagram" />
+        </div>
+        <div className="icon-container">
+          <img className="background-icon" src="\images\Polygon-1-copy-2 2.png" alt="background" />
+          <img className="overlay-icon" src="\images\fe_instagram.png" alt="Instagram" />
+        </div>
+        <div className="icon-container">
+          <img className="background-icon" src="\images\Polygon-1-copy-2 2.png" alt="background" />
+          <img className="overlay-icon" src="\images\fe_instagram.png" alt="Instagram" />
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
