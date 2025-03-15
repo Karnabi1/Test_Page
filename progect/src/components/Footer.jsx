17efb1631e8d666fb5edc25d1252a8c5ab8d9624
Sofiia <nazarenkosofia664@gmail.com>
2025-03-15 20:37:44 +0200

    Трохи змінила файл Footer.jsx, додала там масив та написала для цього файлу CSS код

--- progect/src/components/Footer.jsx
+++ progect/src/components/Footer.jsx
@@ -2,23 +2,19 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="footer">
-      <p>&copy; 2025 Всі права захищені</p>
-      <div className="icons">
-        <div className="icon-container">
-          <img className="background-icon" src="\images\Polygon-1-copy-2 2.png" alt="background" />
-          <img className="overlay-icon" src="\images\fe_instagram.png" alt="Instagram" />
-        </div>
-        <div className="icon-container">
-          <img className="background-icon" src="\images\Polygon-1-copy-2 2.png" alt="background" />
-          <img className="overlay-icon" src="\images\fe_instagram.png" alt="Instagram" />
-        </div>
-        <div className="icon-container">
-          <img className="background-icon" src="\images\Polygon-1-copy-2 2.png" alt="background" />
-          <img className="overlay-icon" src="\images\fe_instagram.png" alt="Instagram" />
+    <div className="footer">
+      <div className="social-links">
+        <p>Let's Be Friends</p>
+        <div className="icons">
+          {Array(3).fill().map((_, index) => (
+            <div className="icon-container" key={index}>
+              <img className="background-icon" src="/images/Polygon-1-copy-2 2.png" alt="background" />
+              <img className="overlay-icon" src="/images/fe_instagram.png" alt="Instagram" />
+            </div>
+          ))}
         </div>
       </div>
-    </footer>
+    </div>
   );
 }
 
